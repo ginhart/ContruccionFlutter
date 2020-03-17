1854d15974efef85edcf81b2199d3c8b2dc54429
Zairi Marion Pineda <53613111+ZairiM@users.noreply.github.com>
2020-03-17 18:22:36 -0500

    update historial view

--- bibliotek/lib/views/historial.dart
+++ bibliotek/lib/views/historial.dart
@@ -17,8 +17,7 @@ class HistorialC {
       'Marcombo', 300, 'https://images-na.ssl-images-amazon.com/images/I/7139EcoIUpL.jpg',1,'00:00');*/
 }
 
-class _HistorialState extends State<Historial> {
-  @override
+class _HistorialState extends State<Historial> {@override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
@@ -27,12 +26,66 @@ class _HistorialState extends State<Historial> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: <Widget>[
-              Image.network(
-                  'https://images-na.ssl-images-amazon.com/images/I/7139EcoIUpL.jpg'),
-              Text('Android Studio'),
-            ],
+          child: Container(
+            child: Column(children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Image.network(
+                          'https://images-na.ssl-images-amazon.com/images/I/7139EcoIUpL.jpg',
+                          height: 300,
+                          width: 200),
+                      Expanded(
+                          child: const Text.rich(
+                              TextSpan(text: '', children: <TextSpan>[
+                        TextSpan(text: 'Nombre: \n', style: TextStyle()),
+                        TextSpan(text: 'Android Studio \n', style: TextStyle()),
+                        TextSpan(text: 'Autor: \n', style: TextStyle()),
+                        TextSpan(
+                            text: 'Jesus Tomas, Vicente Carbonell \n',
+                            style: TextStyle()),
+                        TextSpan(text: 'Editorial: \n', style: TextStyle()),
+                        TextSpan(text: 'Marcombo \n', style: TextStyle()),
+                        TextSpan(text: 'Páginas: \n', style: TextStyle()),
+                        TextSpan(text: '3000 \n', style: TextStyle()),
+                      ])))
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        const Text.rich(TextSpan(text: '', children: <TextSpan>[
+                          TextSpan(text: 'Disponibilidad: \n', style: TextStyle()),
+                          TextSpan(
+                              text: '0 \n', style: TextStyle()),
+                              TextSpan(text: 'Tiempo restante: \n', style: TextStyle()),
+                          TextSpan(
+                              text: '00:00 \n', style: TextStyle()),
+                        ])),
+                      ])
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          IconButton(icon: Icon(Icons.favorite)),
+                          Text('Agregar a Favoritos'),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          IconButton(icon: Icon(Icons.star)),
+                          Text('Agregar a Deseados'),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              )
+            ]),
           ),
         ),
       ),
